4849a0964535d4eb92b77b2d71b0a5e63f195ebd
Scis1437 <90972585+Scis1437@users.noreply.github.com>
2022-12-07 12:22:55 +0700

    update score

--- api/app.js
+++ api/app.js
@@ -4,9 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testAPIRouter = require("./routes/testAPI");
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+// var testAPIRouter = require("./routes/testAPI");
 const dbo = require('./db');
 const { parseArgs } = require('util');
 const db = require('./db');
@@ -14,6 +14,14 @@ const db = require('./db');
 var app = express();
 app.use(cors());
 app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', async function (req, res) {
   // db.users.count(function (err, result) {
   //   if (result <= 0) {
@@ -25,50 +33,78 @@ app.get('/', async function (req, res) {
   // })
   const db = dbo.getDb()
 
-  db
-    .collection('member')
-    .find({}).limit(10)
+  const student = db
+    .collection('station')
+    
+    
+    
+    .find({}).limit(200)
     .toArray((err, result) => {
       if (err) res.status(400).send('Error fetching customers list')
       else res.json(result)
     })
-})
-// var mysql = require("mysql")
-// var { MongoClient } = require('mongodb');
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+    //const Station1 = 
+  })
+  
+  
+  // var mysql = require("mysql")
+  // var { MongoClient } = require('mongodb');
 
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 // app.use("/testAPI", testAPIRouter);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 //mongo
 app.get('/user', function (req, res) {
-  db.users.find(function (err, docs) {
+  const db = dbo.getDb()
+  const users = db.collection('member')
+  users.find(function (err, docs) {
     res.json(docs)
   })
 })
 app.get('/user/:id', function (req, res) {
-  var id = parseInt(req.params.id)
+  var id = Number(req.params.id)
+  const db = dbo.getDb()
+  const users = db.collection('member')
+  users.findOne({ id:id }, function (err, docs) {
+    res.json(docs)
 
-  db.users.findOne({ id: id }, function (err, docs) {
+  })
+})
+
+// app.get('/user/:stationId', (req, res) => {
+//   res.json(user.find(user => user.stationID === req.params.stationId))
+// })
+app.get('/user/:id', function (req, res){
+  var Station1 = Number(req.params.stationID)
+  const db = dbo.getDb()
+  const user = db.collection('member')
+  user.findOne({stationID:stationID}, function(err,docs){
     res.json(docs)
   })
 })
-app.get('/user/:Station1', function (req, res){
-  var Station1 = parseArgs(req.params.Station1)
-  db.user.findOne({Station1:Station1}, function(err,docs){
-    res.json(docs)
-  })
+app.put('/station/:id', async(req, res) => {
+  const filter = { stationID: `${req.params.id}`, ID: `${req.query.std}` };
+  const updateDoc = {
+    $set: {
+      Test1: 5,
+    },
+  };
+  // const option = {promoteValues: true}
+  const updateIndex = await dbo.getDb().collection('station').findOneAndUpdate(filter, updateDoc, {returnDocument: 'after'})
+  res.json({id: req.params.id, update: updateIndex.value})
 })
+
+// app.get('/Station1/:id', function (req, res) {
+//   var id = parseArgs(req.params.id)
+
+//   db.users.findOne({ id:id }, function (err, docs) {
+//     res.json(docs)
+//   })
+// })
 //post
 // error handler
 app.use(function(err, req, res, next) {
--- api/db.js
+++ api/db.js
@@ -9,7 +9,23 @@ const databaseUrl = `mongodb+srv://${user}:${passwd}@testproject.w32eqa7.mongodb
 const client = new MongoClient(databaseUrl)
 
 let dbConnection
-
+// let join = MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("member");
+//   dbo.collection('student').aggregate([
+//     { $lookup:
+//        {
+//          from: 'Station1',
+//          localField: 'Stationid',
+//          foreignField: 'ID',
+//          as: 'Station1 Score'
+//        }
+//      }
+//     ]).toArray(function(err, res) {
+//     if (err) throw err;
+//     console.log(JSON.stringify(res));
+//     db.close();
+// });
 module.exports = {
   connectToServer: function (callback) {
     client.connect(function (err, db) {
@@ -18,6 +34,7 @@ module.exports = {
       }
 
       dbConnection = db.db("member");
+      //dbConnection = db.db("Station1");
       console.log("Successfully connected to MongoDB.");
 
       return callback();
