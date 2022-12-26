d64680b312f77b1086498d228baf19cb36263018
Scis1437 <90972585+Scis1437@users.noreply.github.com>
2022-12-26 21:30:53 +0700

    jwt tokeen

--- api/routes/student.route.js
+++ api/routes/student.route.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const { getAllStudent, addStudent } = require('../controllers/student.controller')
+const verifyJWT = require('../middleware/verifyJWT');
 
 // create router
 const router = express.Router()
 
-router.get('/', getAllStudent)
+router.get('/', verifyJWT,getAllStudent)
+
 router.post('/', addStudent)
 
 module.exports = router
