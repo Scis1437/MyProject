927de9e25299531a7bc66855c716b14e26804a86
Scis1437 <90972585+Scis1437@users.noreply.github.com>
2023-01-04 23:51:36 +0700

    "auth but jwt broke

--- api/index.js
+++ api/index.js
@@ -5,19 +5,24 @@ const bodyParser = require('body-parser');
 const studentRouter = require('./routes/student.route');
 const stationRouter = require('./routes/station.route');
 const testRouter = require('./routes/test.route');
-//const verifyJWT = require('./middleware/verifyJWT');
+const authRouter = require('./routes/auth.route');
+//const useJWT = require('./middleware/verifyJWT');
 const registerRouter = require('./routes/register.route');
 const app = express()
 
 // use middleware
+//app.use(useJWT)
 app.use(cors())
 app.use(bodyParser.json())
+//app.use(cookieParser());
 
 // router
 app.use('/student', studentRouter)
 app.use('/station',stationRouter)
 app.use('/test',testRouter)
 app.use('/register', registerRouter)
+app.use('/auth', require('./routes/auth.route'));
+//app.use('/auth', authRouter)
 // start server
 app.listen(9000, () => {
     console.log('Server started')
--- api/middleware/verifyJWT.js
+++ api/middleware/verifyJWT.js
@@ -1,23 +1,42 @@
-
-const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-
-const verifyJWT = (req ,res, next) =>{
+const verifyJWT = (req, res, next) => {
     const authHeader = req.headers['authorization'];
-    if(!authHeader) return res.sendStatus(401);
-    console.log(authHeader);
+    if (!authHeader) return res.sendStatus(401);
+    console.log(authHeader); // Bearer token
     const token = authHeader.split(' ')[1];
     jwt.verify(
         token,
         process.env.ACCESS_TOKEN_SECRET,
         (err, decoded) => {
-            if(err) return res.sendStatus(403);
-            req.user = decoded,username;
+            if (err) return res.sendStatus(403); //invalid token
+            req.user = decoded.username;
             next();
         }
-    )
+    );
 }
 
 module.exports = verifyJWT
+// const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
+// require('dotenv').config();
+
+
+// const verifyJWT = (req ,res, next) =>{
+//     const authHeader = req.headers['authorization'];
+//     if(!authHeader) return res.sendStatus(401);
+//     console.log(authHeader);
+//     const token = authHeader.split(' ')[1];
+//     jwt.verify(
+//         token,
+//         process.env.ACCESS_TOKEN_SECRET,
+//         (err, decoded) => {
+//             if(err) return res.sendStatus(403);
+//             req.user = decoded,username;
+//             next();
+//         }
+//     )
+// }
+
+// module.exports = verifyJWT
