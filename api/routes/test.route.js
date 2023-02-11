502e699f97475c3a2725ac7a5fcdee2c371fb8db
Scis1437 <90972585+Scis1437@users.noreply.github.com>
2023-02-11 16:32:25 +0700

    some change

--- api/routes/test.route.js
+++ api/routes/test.route.js
@@ -8,5 +8,5 @@ const router = express.Router()
 
 router.get('/',verifyRoles(ROLES_LIST.Admin,ROLES_LIST.Teacher), getAllTest)
 router.post('/',verifyRoles(ROLES_LIST.Admin,ROLES_LIST.Teacher), addTest)
-router.get('/:student_id',verifyRoles(ROLES_LIST.Admin,ROLES_LIST.Teacher,ROLES_LIST.Student),getStudenttest)
+router.get('/:student_id',getStudenttest)
 module.exports = router
