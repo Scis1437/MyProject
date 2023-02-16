882a1dab8bfc00e42fca5fed6fa35a071c0c82a1
Scis1437 <90972585+Scis1437@users.noreply.github.com>
2023-02-16 19:58:11 +0700

    export to excle complete

--- api/controllers/export.controller.js
+++ api/controllers/export.controller.js
@@ -1,31 +1,22 @@
-
-const {prisma} = require('../db')
-const xlsx = require('xlsx')
-const fs = require('fs')
-
-const getDataFromDatabase = async (req,res)=>{
-    const tests = await prisma.test.findMany({})
-    
-    res.json(tests)
-}
-async function generateExcel() {
-  const data = await getDataFromDatabase() // query the database to get the data
-
-  const sheet = xlsx.utils.json_to_sheet(data)
-
-  const workbook = xlsx.utils.book_new()
-  xlsx.utils.book_append_sheet(workbook, sheet, 'Sheet1')
-
-  const excelData = xlsx.write(workbook, { type: 'buffer', bookType: 'xlsx' })
-
-  const fileName = 'data.xlsx'
-  fs.writeFile(fileName, excelData, (err) => {
-    if (err) {
-      console.error(err)
-    } else {
-      console.log(`Excel file saved as ${fileName}`)
-    }
-  })
-}
-
-module.exports = { generateExcel}
+const { prisma } = require("../db");
+const xlsx = require("xlsx");
+const fs = require("fs");
+// const jsonObject = require('/data.json')
+const getDataFromDatabase = async (req, res) => {
+    const data = await prisma.test.findMany({});
+    const jsonData = JSON.stringify(data);
+    await fs.promises.writeFile("data.json", jsonData);
+    console.log("JSON file saved");
+  
+    const workbook = xlsx.utils.book_new();
+    const sheet = xlsx.utils.json_to_sheet(data);
+  
+    xlsx.utils.book_append_sheet(workbook, sheet, "Sheet1");
+  
+    const excelData = xlsx.write(workbook, { type: "buffer", bookType: "xlsx" });
+    await fs.promises.writeFile("data.xlsx", excelData);
+    console.log("Excel file saved");
+  
+    res.sendStatus(200);
+  };
+module.exports = {getDataFromDatabase}
--- api/routes/export.route.js
+++ api/routes/export.route.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const exportController = require('../controllers/export.controller');
 
-router.get('/', exportController.generateExcel);
+router.get('/', exportController.getDataFromDatabase);
 
 module.exports = router;
