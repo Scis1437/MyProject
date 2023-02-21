8ee0b83817c25e5b08cff148c1397ecd41dd22ee
Scis1437 <90972585+Scis1437@users.noreply.github.com>
2023-02-21 21:35:18 +0700

    dynamic export by parses station_id

--- api/controllers/export.controller.js
+++ api/controllers/export.controller.js
@@ -3,13 +3,30 @@ const xlsx = require("xlsx");
 const fs = require("fs");
 // const jsonObject = require('/data.json')
 const getDataFromDatabase = async (req, res) => {
-    const data = await prisma.test.findMany({});
-    const jsonData = JSON.stringify(data);
+  const dataScore = await prisma.test.findMany({
+    where: {
+        
+        station_Id:  req.body.station_id,
+        
+        
+    },
+    select:{
+        student_id:true,
+        station_Id:true,
+        test_number:true,
+        score:true
+    },
+
+      
+})
+    
+
+    const jsonData = JSON.stringify(dataScore);
     await fs.promises.writeFile("data.json", jsonData);
     console.log("JSON file saved");
   
     const workbook = xlsx.utils.book_new();
-    const sheet = xlsx.utils.json_to_sheet(data);
+    const sheet = xlsx.utils.json_to_sheet(dataScore);
   
     xlsx.utils.book_append_sheet(workbook, sheet, "Sheet1");
   
