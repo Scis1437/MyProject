0080cca9314565d2a21581a11dce14e95a423936
Scis1437 <90972585+Scis1437@users.noreply.github.com>
2023-03-07 14:03:25 +0700

    log router and prisma db update

--- api/controllers/logger.controller.js
+++ api/controllers/logger.controller.js
@@ -1,5 +1,10 @@
 const { json } = require("body-parser");
 const { transports, format, createLogger } = require("winston");
+const express = require('express');
+const morgan = require('morgan');
+const fs = require('fs');
+
+const app = express();
 
 const teacherLog = createLogger({
   transports: [
@@ -16,6 +21,7 @@ const teacherLog = createLogger({
         }),
         format.json()
       ),
+      
     }),
     new transports.File({
       filename: "teacher_log_error",
@@ -32,7 +38,16 @@ const teacherLog = createLogger({
         format.json()
       ),
     }),
+    
   ],
 });
 
-module.exports = { teacherLog };
+const readLog = async(req, res) => { fs.readFile('teacher_log', 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    res.status(500).send('An error occurred while reading the log file.');
+  } else {
+    res.send(`<pre>${data}</pre>`);
+  }
+});}
+module.exports = { teacherLog,readLog };
--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express =  require("express");
 const cors = require('cors')
 const bodyParser = require('body-parser');
-
+const logRouter = require('./routes/log.route');
 const studentRouter = require('./routes/student.route');
 const stationRouter = require('./routes/station.route');
 const testRouter = require('./routes/test.route');
@@ -14,14 +14,29 @@ const useJWT = require('./middleware/verifyJWT');
 const cookieParser = require('cookie-parser');
 const registerRouter = require('./routes/register.route');
 const app = express()
+const morgan = require('morgan');
+const fs = require('fs');
 
+
+// app.get('/teacher-log', (req, res) => {
+//     fs.readFile('teacher_log', 'utf-8', (err, data) => {
+//       if (err) {
+//         console.error(err);
+//         res.status(500).send('An error occurred while reading the log file.');
+//       } else {
+//         res.send(`<pre>${data}</pre>`);
+//       }
+//     });
+//   });
+
+app.use(morgan('combined'));
 // use middleware
 //app.use(useJWT)
 app.use(cors())
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(bodyParser.json())
-
+app.use('/teacher-log', logRouter);
 app.use(cookieParser());
 app.use('/register', registerRouter)
 app.use('/auth', authRouter);
