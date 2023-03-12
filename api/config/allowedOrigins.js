3f93083ab040743d36a794a4d0dc81e15c830da6
Scis1437 <90972585+Scis1437@users.noreply.github.com>
2023-03-12 16:31:08 +0700

    allow vercel deployment

--- api/config/allowedOrigins.js
+++ api/config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'https://my-project-khaki-three.vercel.app',
+    'https://my-project-scis1437.vercel.app',
     // 'http://127.0.0.1:5500',
     'http://localhost:9000',
     'http://localhost:3000',
