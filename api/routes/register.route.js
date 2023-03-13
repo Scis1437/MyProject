b554def228729d6d4ff38387115426cc0150e0a5
charnnarong <charnnarong_2542@hotmail.com>
2023-03-13 23:30:00 +0700

    add user

--- api/routes/register.route.js
+++ api/routes/register.route.js
@@ -5,8 +5,8 @@ const ROLES_LIST = require("../config/roles_list");
 const verifyRoles = require("../middleware/verifyRoles");
 const { Role } = require('@prisma/client');
 const router = express.Router();
-router.put('/', verifyRoles(Role.ADMIN,1),changePassword);
-router.post('/', verifyRoles(Role.ADMIN,1) ,handleNewUser);
-router.delete('/', verifyRoles(Role.ADMIN,1),deleteUser);
+router.put('/', verifyRoles(Role.ADMIN),changePassword);
+router.post('/', verifyRoles(Role.ADMIN) ,handleNewUser);
+router.delete('/', verifyRoles(Role.ADMIN),deleteUser);
 
 module.exports = router;
