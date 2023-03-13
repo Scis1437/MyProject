910c457c9bfa4f3f8277607c2e6493fa9c713878
charnnarong <charnnarong_2542@hotmail.com>
2023-03-13 20:30:24 +0700

    push after fix 2

--- client/pages/menu/user.js
+++ client/pages/menu/user.js
@@ -6,7 +6,7 @@ import { useRouter } from "next/router";
 import axios from "axios";
 import Logout from '../../item/logout'
 import { Teacher } from "../../../api/config/roles_list";
-import Logout from "../../item/logout";
+
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL 
 // const users = [
 //   {
