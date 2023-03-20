803f60c24a659bc74df822ada949831169c0f49e
Scis1437 <90972585+Scis1437@users.noreply.github.com>
2023-03-20 21:26:00 +0700

    admin role del

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth.controller');
-
+const { Role } = require('@prisma/client');
+const verifyRoles = require('../middleware/verifyRoles');
 router.post('/', authController.handleLogin);
-router.delete('/', authController.deleteUser)
+router.delete('/', verifyRoles(Role.ADMIN),authController.deleteUser)
 
 module.exports = router;
